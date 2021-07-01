df225e674736cfdcbeaa84c7faade44a8cde4ee5
Abdullah <akhal022@uottawa.ca>
2021-07-01 11:49:03 -0400

    Updated Editor

--- extensions/content-manager/admin/src/components/CustomEditor/index.js
+++ extensions/content-manager/admin/src/components/CustomEditor/index.js
@@ -3,7 +3,7 @@ import { useParams } from "react-router-dom";
 
 function Editor() {
   const { slug, id } = useParams();
-  const src = `http://localhost:3000/editor/${id}`;
+  const src = `https://remembrance-eds-83642.herokuapp.com/editor/${id}`;
   return (
     <>
       <iframe src={src} width="1673" height="880"></iframe>
